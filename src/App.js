8621650b2c7ac83b68ad3433acfea60375815295
Tammivuori <marko.tammivuori@gmail.com>
2020-03-23 12:45:44 +0200

    2.6: puhelinluettelo step1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// HY MOOC 2.6: puhelinluettelo step1
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React, { useState } from 'react'
+
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Marko Tammi'}
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+
+// Eventhandler for Add button. Set a new name to "persons" object.
+const handleAddName = (event) => {
+    event.preventDefault()
+    //console.log('addName clicked', event.target)
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
 }
 
-export default App;
+// Eventhandler to record a new name input typing 
+const handleNameChange =(event) => {
+  // console.log(event.target.value)
+  setNewName(event.target.value)
+}
+
+
+  return (
+    <div>
+      <h3>Phonebook</h3>
+
+      <form onSubmit={handleAddName}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button className="mt-3 btn btn-outline-primary" type="submit">Add</button>
+        </div>
+      </form>
+      
+      <h3 className="mt-5">Numbers</h3>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}  
+
+    </div>
+  )
+} // END OF App()
+
+
+export default App
